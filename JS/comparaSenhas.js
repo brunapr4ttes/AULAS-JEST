6fc57b26b47acfac1c8a993d0d9c18b4399b75dc
Bruna Prates <138784771+brunapr4ttes@users.noreply.github.com>
2024-03-10 16:09:35 -0300

    Atualização aulas-jest

---
+++ JS/comparaSenhas.js
@@ -0,0 +1,10 @@
+function comparaSenhas(param1, param2) {
+    // eslint-disable-next-line eqeqeq
+    if (param1 != param2) {
+      console.log('sucesso');
+    }else{
+      console.log('erro');
+    }
+  };
+  
+  console.log(comparaSenhas(a, b))
